235c7fa86384ed2f2760f0dc59d391acf9808094
Ayoub Debbagh <ayoubdebbagh7@gmail.com>
2025-01-15 02:07:05 +0100

    initializing backend api

---
+++ polling-api/src/test/java/com/polling/polling_api/PollingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polling.polling_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
